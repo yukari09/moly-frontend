93ac57089315e8e87868642a21f8dae903253805
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-08-18 14:48:28 +0800

    Add

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,25 +1,26 @@
-import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { Toaster } from "@/components/ui/sonner";
-import {NextIntlClientProvider, hasLocale} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 
-const inter = Inter({
+const geist = Geist({
   subsets: ["latin"],
   display: "swap",
 });
 
 export const metadata = {
   title: "Next.js Starter Templates and Themes",
-  description: "Discover Next.js templates, starters, and themes to jumpstart your application or website build.",
+  description:
+    "Discover Next.js templates, starters, and themes to jumpstart your application or website build.",
 };
 
 export default async function RootLayout({ children, params }) {
-  const {locale} = await params;
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
@@ -30,16 +31,16 @@ export default async function RootLayout({ children, params }) {
     notFound();
   }
   return (
-    <html lang={locale} className={inter.className}>
+    <html lang={locale} className={geist.className}>
       <body>
-          <AuthProvider>
-            <NextIntlClientProvider locale={locale} messages={messages}>
+        <AuthProvider>
+          <NextIntlClientProvider locale={locale} messages={messages}>
             <Header />
             {children}
             <Footer />
             <Toaster />
-            </NextIntlClientProvider>
-          </AuthProvider>
+          </NextIntlClientProvider>
+        </AuthProvider>
       </body>
     </html>
   );
