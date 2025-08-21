a92cca8a2b2b49cabf8815ae0ef1bcd9578f0339
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-08-22 00:41:05 +0800

    login and register

--- src/app/[locale]/login/page.js
+++ src/app/[locale]/login/page.js
@@ -1,7 +1,8 @@
 import { getTranslations } from "next-intl/server";
 import LoginPageClient from "./LoginPageClient";  
 
-export async function generateMetadata({ params: { locale } }) {
+export async function generateMetadata({ params }) {
+  const { locale } = await(params);
   const t = await getTranslations({ locale, namespace: "LoginPage" });
 
   return {
