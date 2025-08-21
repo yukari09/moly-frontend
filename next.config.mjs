28ff548cd96e55f76653905ec5b91dddd2b41591
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-08-21 13:24:00 +0800

    keyword

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const nextConfig = {};
+const nextConfig = {output: 'standalone'};
  
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
