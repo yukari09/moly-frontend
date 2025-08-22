73a99dfbe10662ce006a2a9dcfc6e8f9c72149c8
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-08-22 21:06:04 +0800

    ja ko language

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -2,7 +2,7 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'ko', 'ja'],
+  locales: ['en', 'ko', 'ja', 'fr', 'es', 'pt', 'de'],
  
   // Used when no locale matches
   defaultLocale: 'en'
