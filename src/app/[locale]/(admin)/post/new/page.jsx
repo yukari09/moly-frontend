dbd139f3fbe6862b781e7c17bfbdc89ef5dffec4
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-10 15:26:14 +0800

    prepare lexical

--- src/app/[locale]/(admin)/post/new/page.jsx
+++ src/app/[locale]/(admin)/post/new/page.jsx
@@ -1,9 +1,6 @@
 'use client';
 
 import { Suspense } from 'react';
-// import { ResourcePage } from '@/components/resource-page';
-// import { columns } from './columns';
-// import { postsDataProvider } from './data-provider';
 
 export default function PostsPage() {
   return (
