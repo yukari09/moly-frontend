99ccd2a748f01b8c144adb6f313bd70d44c48507
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-17 01:10:29 +0800

    add new favicon

--- src/app/[locale]/(main)/page.js
+++ src/app/[locale]/(main)/page.js
@@ -155,10 +155,10 @@ export default async function Home() {
             <div className="container-wrapper">
               <div className="mx-auto flex flex-col items-center gap-2 py-8 text-center md:py-16 lg:py-20 xl:gap-4">
                 <h1 className="text-primary leading-tighter max-w-2xl text-4xl font-semibold tracking-tight text-balance lg:leading-[1.1] lg:font-semibold xl:text-5xl xl:tracking-tighter">
-                  Day Festival And Calendar
+                  Discover the Beauty of World Cultures
                 </h1>
                 <p className="text-foreground max-w-3xl text-base text-balance sm:text-lg">
-                  Clean, modern building blocks. Copy and paste into your apps. Works with all React frameworks. Open Source. Free forever.
+                  Travel beyond borders, embrace diversity, and share unforgettable experiences.
                 </p>
               </div>
             </div>
