9388e71ddd068fed8dfbf1b5a4694da1d0f7e821
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-26 17:55:39 +0800

    Git

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -572,9 +572,9 @@ function SidebarMenuSkeleton({
   ...props
 }) {
   // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`;
-  }, [])
+const [width] = React.useState(() => {
+  return `${Math.floor(Math.random() * 40) + 50}%`;
+});
 
   return (
     <div
