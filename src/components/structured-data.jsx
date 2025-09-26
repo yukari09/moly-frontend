8e40e126e11b17a028e8744a6d781c33de459f88
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-26 20:01:32 +0800

    sdf

--- src/components/structured-data.jsx
+++ src/components/structured-data.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 /**
  * A component to safely render JSON-LD structured data.
  * @param {{data: object}}
  */
 export const StructuredData = ({ data }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <script
       type="application/ld+json"
