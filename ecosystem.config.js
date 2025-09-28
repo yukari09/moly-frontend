21a625804bd9422db6f15b0ef74fd674b1bd9c84
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-28 13:05:38 +0800

    mono

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,7 @@ module.exports = {
       cwd: ".",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
-        NEXT_PUBLIC_TURNSTILE_SITE_KEY: "0x4AAAAAABLfdqrrH9bmKIai",
-        NEXT_PUBLIC_SITE_URL: "https://dattk.com",
-        NEXT_PUBLIC_GA_ID: "G-PYBYBYGL2R",
-        NEXT_PUBLIC_IMAGE_HOST: "https://image.dattk.com"
+        PORT: 3000
       },
     }
   ],
