1334e98639e76cec9a4b5d6bb3b58cedcd59750b
yukari <yukari@yukarideMacBook-Pro-2.local>
2025-09-30 16:04:53 +0800

    optimaze docker build

--- apps/dattk/src/app/api/admin/posts/route.js
+++ apps/dattk/src/app/api/admin/posts/route.js
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import { listPostsOffset, destroyPost } from '@/lib/graphql'
+import { listPostsOffset, destroyPost, updatePost } from '@/lib/graphql'
 import { NextResponse } from 'next/server'
 
 export async function POST(req) {
